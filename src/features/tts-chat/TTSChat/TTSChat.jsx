6f8d6b173fbae14fe515a3e6f22014e19ac4f58c
Никита <nikitval@list.ru>
2025-09-20 16:34:31 +0300

    фыв

--- src/features/tts-chat/TTSChat/TTSChat.jsx
+++ src/features/tts-chat/TTSChat/TTSChat.jsx
@@ -21,7 +21,7 @@ export const TTSChat = ({ volume, twitchVoiceProp }) => {
             if (message?.tags['reply-parent-user-login']) return
             try {
                 console.log('twitchVoice', twitchVoice)
-                const res = await fetch("http://localhost:5001/api/speak", { // ПОМЕНЯТЬ НА ПРОДАКШЕНЕ НА /api/speak
+                const res = await fetch("/api/speak", { // ПОМЕНЯТЬ НА ПРОДАКШЕНЕ НА /api/speak
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({
