c85bddf16d158b1b1bb48a9c7e551c324500263a
Никита <nikitval@list.ru>
2025-10-28 12:18:07 +0300

    qr telegram widget

--- src/app/App.js
+++ src/app/App.js
@@ -3,15 +3,30 @@ import { GlobalPage } from '../pages/GlobalPage/GlobalPage'
 import s from './App.module.scss'
 import { ChatWidget } from '../pages/Widgets/ChatWidget/ChatWidget'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { QRWidget } from '../pages/Widgets/QRWidget/QRWidget'
+import TgLogo from '../shared/assets/icons/telegram-logo-filled.svg'
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID
 
 function App() {
   const location = useLocation()
-  const isWidgetRoute = location.pathname.startsWith('/widget')
+  const starts = (path) => location.pathname.startsWith(path)
 
-  if (isWidgetRoute) {
-    return <ChatWidget />
+  const text = (
+    <div>
+      <h3>Подпишись</h3>
+      <p>Не пропусти стримы</p>
+      <h2>@SALTEIN</h2>
+    </div>
+  )
+
+  if (starts('/widget')) {
+    if (starts('/widget/chat')) {
+      return <ChatWidget />
+    }
+    else if (starts('/widget/qrcode')) {
+      return <QRWidget value={'https://t.me/saltein'} logoImage={TgLogo} text={text} frequency={300} showTime={30} />
+    }
   }
 
   return (
